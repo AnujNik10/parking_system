cc910e5503240e39bf832c3d6a30937bd6066eb2
Siraniket47 <aniketnik47@gmail.com>
2023-04-19 22:58:55 +0530

    Locate changes

--- src/app/coepian/coepian.component.ts
+++ src/app/coepian/coepian.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http'
 
+
 @Component({
   selector: 'app-coepian',
   templateUrl: './coepian.component.html',
--- src/app/locate/locate.component.ts
+++ src/app/locate/locate.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocateComponent implements OnInit {
 
+  slot:any=5
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
+  set1() {
+    this.slot=1
+  }
+  set2() {
+    this.slot=2
+  }
+  set3() {
+    this.slot=3
+  }
+  set4() {
+    this.slot=4
+  }
+  
+
+
 }
+
+
