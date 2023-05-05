1b6f2682b8f247ca522ca990c5e5de2f6b2623c2
Mandar86 <mandarjvategavkar45@gmail.com>
2023-05-05 13:57:16 +0530

    signup backend connect and app component localstorage logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'home';
+
+  constructor(private router:Router){
+    let user = localStorage.getItem("smartUser")
+    if(!user && window.location.pathname != "/signup"){
+      // console.log(window.location.pathname);
+      this.router.navigateByUrl("/login")
+    }
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl, FormControlName} from '@angular/forms';
+import { Router } from '@angular/router';
+import axios from 'axios';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,15 +9,40 @@ import { FormGroup,FormControl, FormControlName} from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
+  constructor(private router: Router) {  }
+  
   signupform = new FormGroup({
-    Username:new FormControl(''),
-    Password:new FormControl(''),
+    email:new FormControl(''),
+    password:new FormControl(''),
+    fname:new FormControl(''),
+    lname:new FormControl(''),
+    phone:new FormControl(''),
   })
 
+  error:String="";
+  
+
   signupUser(){
-    console.warn(this.signupform.value)
+    console.log(this.signupform.value)
+    axios.post("http://localhost:5000/api/user/signup",this.signupform.value)
+    .then((response)=>{
+      console.log(response)
+      if(response.data.user){
+        localStorage.setItem("smartUser",JSON.stringify(response.data.user))
+        console.log(this.router);
+        this.router.navigateByUrl("/")
+        
+      }
+    })
+    .catch((err)=>{
+      let errMsg = err.response.data.errors[0].msg
+      if(errMsg){
+        this.error = errMsg
+      }
+    })
+    
   }
-  constructor() { }
+  
 
   ngOnInit(): void {
   }
